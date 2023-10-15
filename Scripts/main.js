c54d4ccbbdfea8824e8c3e02f150d4faebd3758d
Pdatecla <146305837+Pdatecla@users.noreply.github.com>
2023-10-15 01:43:07 -0300

    Add files via upload
    Signed-off-by: Pdatecla <146305837+Pdatecla@users.noreply.github.com>

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,14 @@
 window.alert("Reservá ya tu entrada para la próxima función");
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var name = document.getElementById('name').value; 
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    console.log('Nombre: ' + name);
+    console.log('Correo electrónico: ' + email);
+    console.log('Mensaje: ' + message);
+    document.getElementById('contactForm').reset();
+});
